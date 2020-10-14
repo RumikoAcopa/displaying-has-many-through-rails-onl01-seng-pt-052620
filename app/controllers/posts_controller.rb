4b7a0981247c7d071e1d78779709a59e4b31e44b
rumikoacopa <rumikorios@gmail.com>
2020-10-13 22:09:18 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,9 @@
 class PostsController < ApplicationController
 
   def show
-    @post = Post.find(params[:id])
+    @post = Post.find(params[:id]) # define a show action that finds a particular post to make it available for display.
+
+
   end
 end
+#display all of the comments on a particular post. We'll include the username of the user who created the comment as well as a link to their show page.
